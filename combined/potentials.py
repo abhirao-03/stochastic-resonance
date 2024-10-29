707d1deca8a159feab42361922d5739db79a8779
ToddDavison <s2079059@ed.ac.uk>
2024-10-29 15:49:00 +0000

    updating

--- combined/potentials.py
+++ combined/potentials.py
@@ -7,13 +7,12 @@ def poor_potential(x):
     return (x**4)/4 - (850.5/3)*x**3 + (241091.72/2)*x**2 - 22778441.868*x +1613920200.24048
 
 def stable_potential(x):
-    a = 1.1
-    b = 3.4
-    c = -3.0
+    a = 0.01
+    b = 0
 
-    X_temp = a*(x - 283.3)**4 + b*(x - 283.3)**2
+    X_temp = a*(x - 278.6)**2*(x - 288.6)**2+b
 
-    return X_temp + c
+    return X_temp
 
 def sin_potential(x, t):
     a = 1.1
--- combined/sde_models.py
+++ combined/sde_models.py
@@ -59,14 +59,16 @@ class climate_sde(model_params):
         return 1.0
     
 class climate_sde_2(model_params):
-    def __init__(self, T, epsilon=0.2):
-        super().__init__(x_init=1.0, dt = 0.1, time_horizon=1000.0, num_trajectories=1)
-        self.T = T
+    def __init__(self, T=100000, epsilon=0.2):
+        self.time_horizon = 1000
+        super().__init__(x_init=288.6, dt = 0.1, time_horizon=self.time_horizon, num_trajectories=1)
         self.epsilon = epsilon
+        self.T = T
         self.noise = random.normal(loc=0.0, scale=epsilon*self.dt, size=(self.num_steps, self.num_trajectories))
 
     def mu(self, x, t):
-        return -grad(sin_potential, argnums=(0))(x, t)-0.1*(np.sin((2*np.pi*t)/self.T))
+        Q=0.1
+        return -grad(stable_potential, argnums=(0))(x)-Q*(np.sin((2*np.pi*t)/self.T))
     
     def sigma(self, x, t):
         return 1
