84cff2062d88a74107bbc1b1b83abf8dc1713d51
Abhijeet Rao <abhijeetraop@gmail.com>
2024-11-11 16:57:26 +0000

    messing around with epsilon

--- combined/simplified.py
+++ combined/simplified.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def d_poly__d_x(x, t, period=1000):
+def d_poly__d_x(x, t, period=100):
     a1 = 0
     a2 = -3.2
     sin_scale = 3
@@ -11,7 +11,7 @@ def d_poly__d_x(x, t, period=1000):
     a5 = -(3/5) * a3
     a6 = 1
 
-    all_scale = 1.13
+    all_scale = 1.3
     
     return all_scale*(a6*6*x**5 + a5*5*x**4 + a4*4*x**3 + a3*3*x**2 + a2*2*x + a1)
 
@@ -29,16 +29,16 @@ def sin_potential_derivative(x, t, period=1000):
     return stationary_derivative + osc_derivative
 
 
-np.random.seed(1234567890)
 x_init = 0.0
-epsilon = 0.6
+epsilon = 0.4
 dt = 0.1
-time_horizon = 1000
+time_horizon = 100
 num_steps = int(time_horizon/dt)
 noise = np.random.normal(loc=0.0, scale=epsilon*dt, size=(num_steps,))
-x_val = np.zeros(shape=(num_steps, ))
+x_poly = np.zeros(shape=(num_steps, ))
 time_vec = np.linspace(0, time_horizon, num=num_steps + 1)
-x_val[0] = x_init
+x_poly[0] = x_init
+
 
 
 def mu(x, t):
@@ -50,18 +50,25 @@ def sigma(_x, _t):
 def euler_maruyama():
     for i in range(num_steps - 1):
         t = time_vec[i]
-        x = x_val[i]
+        x = x_poly[i]
 
         dW = noise[i+1] - noise[i]
 
-        x_val[i+1] = x + mu(x, t) * dt + sigma(x, t) * dW
+        x_poly[i+1] = x + mu(x, t) * dt + sigma(x, t) * dW
+
 
         print(f"On iteration {i}")
-    return x_val
-
-x_val = euler_maruyama()
+    return x_poly
 
 
-plt.plot(time_vec[:-1], x_val)
+x_poly = euler_maruyama()
+
+
+plt.plot(time_vec[:-1], x_poly, label = 'polynomial]')
+plt.legend()
+plt.xlabel('$t$')
+plt.ylabel('$X(t)$')
 plt.ylim((-2, 2))
+plt.tight_layout()
+plt.show()
 print()
