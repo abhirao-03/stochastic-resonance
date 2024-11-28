65094a6e3dbd05e68f93ba748ab97c2d0bebea16
s2230095 <s2230095@ed.ac.uk>
2024-11-28 11:26:56 +0000

    removed unused potential

--- potentials.py
+++ potentials.py
@@ -13,16 +13,3 @@ def d_poly__d_x(x, t, period=100):
     all_scale = 1.13
     
     return all_scale*(a6*6*x**5 + a5*5*x**4 + a4*4*x**3 + a3*3*x**2 + a2*2*x + a1)
-
-def inst_switch(x, t, period=1000):
-    a0 = 0
-    a1 = 0
-    a2 = -3.2
-    a3 = 0.25 if t <= period/2 else 0.75
-    a4 = 0.1
-    a5 = -(3/5) * a3
-    a6 = 1
-
-    all_scale = 0.8
-
-    return all_scale*(a6*x**6 + a5*x**5 + a4*x**4 + a3*x**3 + a2*x**2 + a1*x + a0)
