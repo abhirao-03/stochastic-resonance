ef9715850b4c6c187a54e906c66da69c83e80baa
Abhijeet Rao <abhijeetraop@gmail.com>
2025-02-11 20:33:16 +0000

    working on inhouse nelder mead optimisation method.

--- in_house_nelder_mead.py
+++ in_house_nelder_mead.py
@@ -2,23 +2,58 @@ import numpy as np
 import numpy.random as random
 from optimiser import run
 
+x_init = -1
+dt = 0.01
+time_horizon = 1000
+jump_mult = 3
+num_trajectories = 1000
 
-eps_init = 3.7
-dt_init = 0.01
-time_hor_init = 1000
+initial_simplex = np.array([
+                            [x_init,        dt,         time_horizon,       jump_mult,     num_trajectories],
+                            [x_init + 0.01, dt,         time_horizon,       jump_mult,     num_trajectories],
+                            [x_init,        dt - 0.001, time_horizon,       jump_mult,     num_trajectories],
+                            [x_init,        dt,         time_horizon + 100, jump_mult,     num_trajectories],
+                            [x_init,        dt,         time_horizon,       jump_mult + 1, num_trajectories],
+                            [x_init,        dt,         time_horizon,       jump_mult,     num_trajectories - 10]
+                            ])
 
-eps_pert = 0.5
-dt_pert = 0.9
-time_hor_pert = 100
+p_vals = np.empty(shape=(initial_simplex.shape[0]))
 
-initial_simplex = np.array([[eps_init, dt_init, time_hor_init],
-                            [eps_init + eps_pert, dt_init, time_hor_init],
-                            [eps_init, dt_init*dt_pert, time_hor_init],
-                            [eps_init, dt_init, time_hor_init + time_hor_pert]])
-
-p_vals = np.empty(shape=(initial_simplex.shape[0], 2))
 for idx, vertex in enumerate(initial_simplex):
-    p_vals[idx] = np.array([run(vertex), vertex])
+    p_vals[idx] = run(vertex)
 
+idx = np.argsort(p_vals)
+idx = idx[::-1]
+sorted_p_vals = p_vals[idx]
+sorted_simplex = initial_simplex[idx]
 
+def calculate_centroid(simplex):
+    centroid = np.mean(simplex[:-1], axis=0)
+    return centroid
+
+def reflect(centroid, worst_point, alpha=1.0):
+    reflection = centroid + alpha*(centroid - worst_point)
+    return reflection
+
+def expand(centroid, reflected_point, gamma=2.0):
+    expansion = centroid + gamma*(reflected_point - centroid)
+    return expansion
+
+def contract_outside(centroid, reflected_point, beta=0.5):
+    cont_out = centroid + beta*(reflected_point - centroid)
+    return cont_out
+
+def contract_inside(centroid, worst_point, beta=0.5):
+    cont_in = centroid + beta*(worst_point - centroid)
+    return cont_in
+
+def shrink(simplex, best_vertex, sigma=0.5):
+    new_simplex = np.empty(simplex.shape)
+    new_simplex[0] = best_vertex
+
+    new_vertex = best_vertex + sigma*(simplex[1:] - best_vertex)
+    new_simplex[1:] = new_vertex
+    
+    return new_simplex
+    
 print()
--- optimiser.py
+++ optimiser.py
@@ -4,25 +4,7 @@ from numpy import random
 from tqdm import tqdm
 import scipy.stats as stats
 
-def run(vertex:np.array):
-    
-    # dt = 0.01
-    # time_horizon = 1000
-
-    dt = vertex[0]
-    time_horizon = vertex[1]
-
-    num_steps = int(time_horizon/dt)
-
-    num_trajectories = 1000
-
-    x_init = -1
-    jump_mult = vertex[2]
-    epsilon = (4.29 * 2 / np.log(time_horizon)) * jump_mult
-
-    t = np.linspace(0, time_horizon, num_steps)
-    
-    def const_neg_potential(x, t, period=100):
+def const_neg_potential(x, t, period=100):
         """Defines the potential function."""
         t = 25
         a1, a2, a3, a4, a5, a6 = 0, -3.2, 3*np.sin(2*np.pi*t/period), 0.1, -(3/5)*3*np.sin(2*np.pi*t/period), 1
@@ -30,75 +12,81 @@ def run(vertex:np.array):
         
         return all_scale * (a6*6*x**5 + a5*5*x**4 + a4*4*x**3 + a3*3*x**2 + a2*2*x + a1)
 
-    def mu(x, t):
-        return -const_neg_potential(x, t)
+def mu(x, t):
+    return -const_neg_potential(x, t)
 
-    def sigma(x, t):
-        return epsilon ** (1/2)
+def sigma(x, t, epsilon):
+    return (epsilon) ** (1/2)
 
 
-    def simulate(x_init, dt, num_trajectories, num_steps, time_vec, delta=6):
-        jump_times = np.empty((num_trajectories,))
-        x = np.zeros((num_steps,))
-        x[0] = x_init
+def simulate(x_init, dt, time_horizon, jump_mult, num_trajectories: int, delta=6):
+    num_trajectories = int(num_trajectories)
+    num_steps = int(time_horizon/dt)
+    jump_times = np.empty((num_trajectories,))
+    x = np.zeros((num_steps,))
+    x[0] = x_init
+    time_vec = np.linspace(0, time_horizon, num_steps)
+
+    global epsilon
+    epsilon = ((4.29 * 2)/np.log(time_horizon)) * jump_mult
+
+    for j in tqdm(range(num_trajectories)):
+        sign_change_detected = False
+        steps_since_change = 0
+        initial_sign = np.sign(x[0])
+        potential_jump_time = None
         
-        for j in tqdm(range(num_trajectories)):
-            sign_change_detected = False
-            steps_since_change = 0
-            initial_sign = np.sign(x[0])
-            potential_jump_time = None
+        for i in range(num_steps - 1):
+            curr_t = time_vec[i]
+            curr_x = x[i]
+            dW = random.normal(loc=0.0, scale=dt**(1/2))
             
-            for i in range(num_steps - 1):
-                curr_t = time_vec[i]
-                curr_x = x[i]
-                dW = random.normal(loc=0.0, scale=dt**(1/2))
-                
-                x[i+1] = (curr_x + 
-                        mu(curr_x, curr_t) * dt + 
-                        sigma(curr_x, curr_t) * dW)
-                
-                # If we haven't detected a sign change yet
-                if not sign_change_detected:
-                    if np.sign(x[i+1]) != initial_sign:
-                        sign_change_detected = True
-                        steps_since_change = 0
-                        potential_jump_time = curr_t
-                
-                # If we're monitoring a potential zone transition
-                elif steps_since_change < delta:
-                    steps_since_change += 1
-                    # If sign reverts back to initial sign, cancel the monitoring
-                    if np.sign(x[i+1]) == initial_sign:
-                        sign_change_detected = False
-                        steps_since_change = 0
-                        potential_jump_time = None
-                    
-                    # If we've monitored for delta steps and sign change persists
-                    if steps_since_change == delta:
-                        jump_times[j] = potential_jump_time
-                        break
+            x[i+1] = (curr_x + 
+                    mu(curr_x, curr_t) * dt + 
+                    sigma(curr_x, curr_t, epsilon) * dW)
             
-            # If we reach the end without confirming a transition
-            if not sign_change_detected or steps_since_change < delta:
-                jump_times[j] = np.nan
+            # If we haven't detected a sign change yet
+            if not sign_change_detected:
+                if np.sign(x[i+1]) != initial_sign:
+                    sign_change_detected = True
+                    steps_since_change = 0
+                    potential_jump_time = curr_t
+            
+            # If we're monitoring a potential zone transition
+            elif steps_since_change < delta:
+                steps_since_change += 1
+                # If sign reverts back to initial sign, cancel the monitoring
+                if np.sign(x[i+1]) == initial_sign:
+                    sign_change_detected = False
+                    steps_since_change = 0
+                    potential_jump_time = None
+                
+                # If we've monitored for delta steps and sign change persists
+                if steps_since_change == delta:
+                    jump_times[j] = potential_jump_time
+                    break
         
-        return x, jump_times
+        # If we reach the end without confirming a transition
+        if not sign_change_detected or steps_since_change < delta:
+            jump_times[j] = np.nan
+    
+    return x, jump_times
 
-    print('simulating')
-    _, jump_times = simulate(x_init, dt, num_trajectories, num_steps, t)
-
+def exp_cdf(x):
     theoretical_rate = 1/(np.exp(1.17 * 2/epsilon))
+    return 1 - np.exp(-theoretical_rate * x)
 
-    def exp_cdf(x, rate=theoretical_rate):
-        return 1 - np.exp(-rate * x)
+def run(vertex):
+    x_init =  vertex[0]
+    dt = vertex[1]
+    time_horizon = vertex[2]
+    jump_mult = vertex[3]
+    num_trajectories = vertex[4]
+    
+    _, jump_times = simulate(x_init, dt, time_horizon, jump_mult, num_trajectories)
 
-    x_uniformed = exp_cdf(jump_times)
+    x_transformed = exp_cdf(jump_times)
 
-    pvalue = stats.cramervonmises(x_uniformed, 'uniform').pvalue
-    print()
-    print(f'p_value: {pvalue}')
-    print(f'dt: {dt}')
-    print(f'time horizon: {time_horizon}')
-    print(f'jump_mult: {jump_mult}')
-    print()
-    return 1 - pvalue
+    met = stats.cramervonmises(x_transformed, 'uniform').pvalue
+
+    return met
