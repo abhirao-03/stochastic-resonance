2cc760168d544a78e96c4743db25466398000180
s2230095 <s2230095@ed.ac.uk>
2025-02-20 19:17:59 +0000

    new figures!!!

--- potentials.py
+++ potentials.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 def d_poly__d_x(x, t, period=100):
+    t=0
     a1 = 0
     a2 = -3.2
     sin_scale = 3
@@ -9,7 +10,7 @@ def d_poly__d_x(x, t, period=100):
     a5 = -(3/5) * a3
     a6 = 1
 
-    all_scale = 1.13
+    all_scale = 1.3
     
     return all_scale*(a6*6*x**5 + a5*5*x**4 + a4*4*x**3 + a3*3*x**2 + a2*2*x + a1)
 
